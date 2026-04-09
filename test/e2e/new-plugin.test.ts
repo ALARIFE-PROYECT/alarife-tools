a20437e617f5a701a63016ac186fb9e249ff307d
Soria Garcia, Jose Eduardo <pepesoriagarcia99@gmail.com>
2026-04-09 15:08:58 +0200

    fix: test correction

--- test/e2e/new-plugin.test.ts
+++ test/e2e/new-plugin.test.ts
@@ -1,8 +1,11 @@
 import { describe, it, beforeEach, afterEach, mock } from 'node:test';
 import assert from 'node:assert/strict';
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync, mkdirSync } from 'fs';
 import { join } from 'path';
 import newPlugin from '../../src/handlers/new-plugin.js';
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const child_process = require('child_process');
 
 /**
  * Rules:
@@ -18,6 +21,37 @@ import newPlugin from '../../src/handlers/new-plugin.js';
  */
 
 const MOCK_DIR = join(__dirname, '..', 'mock', 'new-plugin');
+mkdirSync(MOCK_DIR, { recursive: true });
+
+const SKELETON_PACKAGE_JSON = JSON.stringify({
+  name: '{PACKAGE_NAME}',
+  version: '0.1.0',
+  description: '{PACKAGE_DESCRIPTION}',
+  author: '{AUTHOR_NAME} <{AUTHOR_EMAIL}>',
+  main: './lib/index.js',
+  scripts: {
+    build: 'tsc'
+  },
+  dependencies: {
+    '{LIBRARY_NAME}': '^0.1.0'
+  }
+}, null, 2);
+
+const SKELETON_README = `# {PACKAGE_NAME}
+
+{PACKAGE_DESCRIPTION}
+
+{PLUGIN_RESUME}
+
+## Author
+
+ [{AUTHOR_NAME}](mailto:{AUTHOR_EMAIL})
+`;
+
+const SKELETON_LICENSE = `Apache License 2.0
+
+Copyright 2026 {AUTHOR_NAME} <{AUTHOR_EMAIL}>
+`;
 
 const OPTIONS = {
   authorName: 'Jose Eduardo Soria Garcia',
@@ -34,7 +68,32 @@ const createEvent = (target: string, options: Record<string, any> = {}): any => 
   options: { ...OPTIONS, ...options }
 });
 
+const writeSkeleton = () => {
+  writeFileSync(join(MOCK_DIR, 'package.json'), SKELETON_PACKAGE_JSON, 'utf8');
+  writeFileSync(join(MOCK_DIR, 'README.md'), SKELETON_README, 'utf8');
+  writeFileSync(join(MOCK_DIR, 'LICENSE'), SKELETON_LICENSE, 'utf8');
+};
+
 describe('new-plugin handler', () => {
+  beforeEach(() => {
+    mock.method(child_process, 'execSync', (command: string) => {
+      if (typeof command === 'string' && command.startsWith('git clone')) {
+        const match = command.match(/"([^"]+)"$/);
+        if (match) {
+          const pluginDir = join(match[1], 'plugin');
+          mkdirSync(pluginDir, { recursive: true });
+          writeFileSync(join(pluginDir, 'package.json'), SKELETON_PACKAGE_JSON, 'utf8');
+          writeFileSync(join(pluginDir, 'README.md'), SKELETON_README, 'utf8');
+          writeFileSync(join(pluginDir, 'LICENSE'), SKELETON_LICENSE, 'utf8');
+        }
+      }
+    });
+  });
+
+  afterEach(() => {
+    mock.restoreAll();
+  });
+
   it('should replace all placeholders in package.json', () => {
     newPlugin(createEvent(MOCK_DIR), null as any, null as any);
 
